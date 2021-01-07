e7e78682e0bacc5b08e8c718b463f293111ed9d4
liyudong <1768756152@qq.com>
2021-01-07 23:31:10 +0800

    feat: 初始化子工程

---
+++ common/src/main/java/therever/common/SpringContextUtil.java
@@ -0,0 +1,23 @@
+package therever.common;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextUtil.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(String beanName, Class<T> beanTye) {
+        return applicationContext.getBean(beanName, beanTye);
+
+    }
+
+}

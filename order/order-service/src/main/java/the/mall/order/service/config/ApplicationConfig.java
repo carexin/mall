df7fab903ee6db13e1856f1a494014665242ec4a
liyudong <1768756152@qq.com>
2021-02-16 21:19:16 +0800

    feat: pay调用order调试

--- order/order-service/src/main/java/the/mall/order/service/config/ApplicationConfig.java
+++ order/order-service/src/main/java/the/mall/order/service/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package the.mall.order.service.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationConfig {
 
 	@Bean
+	@LoadBalanced// 客户端负载均衡, todo 如何通过一个注解就让restTemplate扩展了功能?
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- payment/src/main/java/the/mall/payment/testcontroller/TestPayController.java
+++ payment/src/main/java/the/mall/payment/testcontroller/TestPayController.java
@@ -1,10 +1,13 @@
 package the.mall.payment.testcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("payment/test")
@@ -15,10 +18,20 @@ public class TestPayController {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Resource
+	private DiscoveryClient discoveryClient;
+
 	@GetMapping("get")
 	public OrderDTO get() {
 
 		OrderDTO orderDTO = restTemplate.postForEntity(orderUrl, "no1112", OrderDTO.class).getBody();
 		return orderDTO;
+	}
+
+	@GetMapping("discover")
+	public Object discover() {
+		// discoveryClient.getInstances("PAYMENT-SERVICE");
+
+		return discoveryClient.getServices();
 	}
 }
